eb6bed69bdd32de79872efdac4c300ee5d616936
87969 <87969@farnborough.ac.uk>
2019-09-27 14:24:06 +0100

    Add .m2 Folder

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,24 +9,16 @@ public class Main {
     public static Connection db = null;
 
     public static void main(String[] args) {
-
         // Opens Database
         openDatabase("Inventory Database.db");
-
         //Code for manipulating the data in the database
-
         PurchaseOrderDetails_Controller.ReadPurchasesOrderDetails();
-
         closeDatabase();
-
-
-
     }
 
 
 
  private static void openDatabase(String dbFile){
-
         //Creates a connection to the database
         try{
             Class.forName("org.sqlite.JDBC");
@@ -34,32 +26,19 @@ public class Main {
             config.enforceForeignKeys(true);
             db = DriverManager.getConnection("jdbc:sqlite:resources/" + dbFile, config.toProperties());
             System.out.println("Successful Connection");
-
-
         //Catches any errors from connecting to the database
-        }catch (Exception e){
+        }catch (Exception e) {
             System.out.println("Connection error: " + e);
         }
-
-
     }
-
     private static void closeDatabase(){
-
         //Disconnects the database
         try{
-
             db.close();
             System.out.println("Successful Disconnection");
-
-
-
         //Catches any errors from disconnecting the database
         }catch (Exception e){
             System.out.println("Disconnection error: " + e);
-
         }
     }
-
-
 }
